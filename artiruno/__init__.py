5e1952c308cd82b6dcfdf16b088e0ba6418edbe8
Kodi Arfer <git@arfer.net>
2020-08-12 16:59:42 -0400

    [__init__] Ensure submodules can be accessed from outside.

--- artiruno/__init__.py
+++ artiruno/__init__.py
@@ -1,3 +1,13 @@
+# Ensure that each module can accessed explicitly as `m`; e.g.,
+# `artiruno.m.vda` will get the `vda` module. This is useful because
+# `artiruno.vda` will be set to a function instead of the module.
+import artiruno.preorder, artiruno.vda, artiruno.util
+class C: pass
+m = C()
+for s in ('preorder', 'vda', 'util'):
+    setattr(m, s, getattr(artiruno, s))
+
+# Now do the ordinary imports.
 from artiruno.preorder import (
     PreorderedSet, IC, EQ, LT, GT, ContradictionError)
 from artiruno.vda import vda, Goal
