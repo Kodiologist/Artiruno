Kodiologist/Artiruno

commit 49df8d2fa0cb1a7bed4f1a68e79959734f5744be

[vda] Add items to the preferences preorder as needed.
This change increases speed and allows the use of item spaces that would be too large to represent explicitly.

artiruno/vda.py
@@ -4,7 +4,7 @@ from artiruno.util import cmp, choose2
 
 Goal = enum.Enum('Goal', 'FIND_BEST RANK_ALTS RANK_SPACE')
 
-def vda(criteria, alts, asker, goal):
+def vda(criteria = (), alts = (), asker = None, goal = Goal.FIND_BEST):
     """
 - `criteria` is an iterable of iterables specifying the levels of
   each criterion. With a criterion, we assume that later levels are
@@ -16,11 +16,10 @@ def vda(criteria, alts, asker, goal):
     LT (-1) if b is better
     0 (EQ) if they're equally good"""
 
-    assert isinstance(goal, Goal)
-
-    criteria, alts, item_space, prefs = _setup(criteria, alts)
+    criteria, alts, prefs = _setup(criteria, alts, goal)
 
     def get_pref(a, b):
+        add_items(criteria, prefs, [a, b])
         if (rel := prefs.cmp(a, b)) is None:
             prefs.learn(a, b, rel := asker(a, b))
         return rel
@@ -32,8 +31,7 @@ def vda(criteria, alts, asker, goal):
            value if i == criterion else c[-1]
            for i, c in enumerate(criteria))
 
-    to_try = set(choose2(
-        item_space if goal == Goal.RANK_SPACE else alts))
+    to_try = set(choose2(alts))
     focus = None
 
     while not (goal == Goal.FIND_BEST and len(prefs.maxes(alts)) == 1):
@@ -80,31 +78,43 @@ def vda(criteria, alts, asker, goal):
 
     return prefs
 
-def _setup(criteria, alts):
+def _setup(criteria = (), alts = (), goal = Goal.FIND_BEST):
     "Some initial VDA logic put into its own function so it can be tested separately."
+
+    assert isinstance(goal, Goal)
 
     criteria = tuple(map(tuple, criteria))
     assert all(
         len(c) > 0 and len(c) == len(set(c))
         for c in criteria)
-    alts = tuple(map(tuple, alts))
-    assert all(
-        len(a) == len(criteria) and all(
-            a[i] in criteria[i]
-            for i in range(len(a)))
-        for a in alts)
+
+    if goal == Goal.RANK_SPACE:
+        alts = tuple(itertools.product(*criteria))
+    else:
+        alts = tuple(map(tuple, alts))
+        assert all(
+            len(a) == len(criteria) and all(
+                a[i] in criteria[i]
+                for i in range(len(a)))
+            for a in alts)
 
     # Define the user's preferences as a preorder, with `a < b` if `b`
     # is preferred to `a`, and `a` and `b` incomparable if the user's
-    # preference isn't yet known. We initialize it with the assumption
-    # that on any single criterion, bigger values are better.
-    item_space = tuple(itertools.product(*criteria))
-    prefs = PreorderedSet(item_space)
-    for a, b in choose2(item_space):
-        if sum(l := [x != y for x, y in zip(a, b)]) == 1:
-            ci = l.index(True)
-            prefs.learn(a, b, cmp(
-                criteria[ci].index(a[ci]),
-                criteria[ci].index(b[ci])))
+    # preference isn't yet known.
+    prefs = PreorderedSet()
+    add_items(criteria, prefs, alts)
 
-    return criteria, alts, item_space, prefs
+    return criteria, alts, prefs
+
+def add_items(criteria, prefs, items):
+    # Enforce the assumption that on any single criterion, bigger
+    # values are better.
+    for x in items:
+        if x not in prefs.elements:
+            prefs.add(x)
+            for a in prefs.elements:
+                if sum(l := [e1 != e2 for e1, e2 in zip(x, a)]) == 1:
+                    ci = l.index(True)
+                    prefs.learn(x, a, cmp(
+                        criteria[ci].index(x[ci]),
+                        criteria[ci].index(a[ci])))

tests/test_vda.py
@@ -9,7 +9,7 @@ def test_assumptions():
 
     *_, prefs = artiruno.m.vda._setup(
         criteria = ['abcd', ('bad', 'okay', 'good')],
-        alts = [])
+        goal = Goal.RANK_SPACE)
 
     assert prefs.cmp(('a', 'bad'), ('c', 'bad')) == LT
     assert prefs.cmp(('a', 'okay'), ('a', 'good')) == LT
@@ -111,3 +111,21 @@ def test_simple_strings():
                 ('good', 'expensive'), ('good', 'cheap'))
             for (ai, a), (bi, b) in choose2(enumerate(ranking)):
                 assert prefs.cmp(a, b) == cmp(ai, bi)
+
+def test_big_item_space():
+    n_criteria = 10
+    n_levels = 20
+
+    criteria = [list(range(n_levels))] * n_criteria
+    alts = [[n_levels - 1] * n_criteria for _ in range(2)]
+    alts[0][3] = 15
+    alts[0][6] = 14
+    alts[1][7] = 8
+    alts[1][9] = 4
+
+    prefs = vda(
+        criteria = criteria,
+        alts = alts,
+        asker = cmp,
+        goal = Goal.FIND_BEST)
+    assert prefs.cmp(tuple(alts[0]), tuple(alts[1])) == LT

commit cd672dd501adb8d3a458c93488c5e6342e11be1b

[vda] Fixed enforcement of the rule of dominance.
This bug was a regression caused by 49df8d2fa0cb1a7bed4f1a68e79959734f5744be.

artiruno/vda.py
@@ -117,12 +117,14 @@ def _setup(criteria = (), alts = (), goal = Goal.FIND_BEST):
 def add_items(criteria, prefs, items):
     # Enforce the assumption that on any single criterion, bigger
     # values are better.
-    for x in items:
-        if x not in prefs.elements:
-            prefs.add(x)
-            for a in prefs.elements:
-                if sum(l := [e1 != e2 for e1, e2 in zip(x, a)]) == 1:
-                    ci = l.index(True)
-                    prefs.learn(x, a, cmp(
-                        criteria[ci].index(x[ci]),
-                        criteria[ci].index(a[ci])))
+    for x in set(items) - prefs.elements:
+        prefs.add(x)
+        for a in prefs.elements - {x}:
+            cmps = [
+                cmp(criteria[ci].index(x[ci]),
+                    criteria[ci].index(a[ci]))
+                for ci in range(len(criteria))]
+            if not (LT in cmps and GT in cmps):
+                # One item dominates the other. (We know that `cmps`
+                # isn't all EQ because `x` and `a` are different.)
+                prefs.learn(x, a, LT if LT in cmps else GT)

tests/test_vda.py
@@ -1,4 +1,5 @@
 import random, itertools
+from collections import Counter
 import artiruno
 from artiruno import IC, LT, EQ, GT, vda, Goal, cmp, choose2
 
@@ -10,12 +11,16 @@ def test_assumptions():
     *_, prefs = artiruno.m.vda._setup(
         criteria = ['abcd', ('bad', 'okay', 'good')],
         goal = Goal.RANK_SPACE)
-
     assert prefs.cmp(('a', 'bad'), ('c', 'bad')) == LT
     assert prefs.cmp(('a', 'okay'), ('a', 'good')) == LT
     assert prefs.cmp(('d', 'good'), ('a', 'bad')) == GT
     assert prefs.cmp(('c', 'okay'), ('b', 'bad')) == GT
     assert prefs.cmp(('b', 'good'), ('c', 'okay')) == IC
+
+    *_, prefs = artiruno.m.vda._setup(
+        criteria = [range(3) for _ in range(3)],
+        alts = ((1, 1, 1), (2, 1, 2)))
+    assert prefs.cmp((1, 1, 1), (2, 1, 2)) == LT
 
 def test_appendixD():
     # Appendix D of Larichev and Moshkovich (1995).
@@ -175,15 +180,17 @@ def test_conclusivity(diag):
             (1300, 'DVO', '0000'),
             (1700, 'DVO', '1100')))
 
-    if diag:
-        from collections import Counter
-        print('Number of questions required:',
-            Counter(len(r['questions']) for r in result))
-        print('Maxima:', Counter(r['maxes'] for r in result))
-
     # Every sequence of LT or GT choices should lead to a single best
     # item.
     assert all(len(r['maxes']) == 1 for r in result)
+
+    # If changes to Artiruno result in having to ask less questions,
+    # change this test. Having to ask more questions is a regression.
+    assert (Counter(len(r['questions']) for r in result) ==
+        Counter({4: 16, 3: 4, 2: 2}))
+
+    if diag:
+        print('Maxima:', Counter(r['maxes'] for r in result))
 
 def test_recode_criteria():
     # Renaming criterion values shouldn't change the questions asked