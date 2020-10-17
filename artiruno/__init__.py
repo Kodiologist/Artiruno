c3c56132b8be1230e71926fa044ebbb990b7912d
Kodi Arfer <git@arfer.net>
2020-10-17 10:11:40 -0400

    [aritruno.__init__] Small cleanup.

--- artiruno/__init__.py
+++ artiruno/__init__.py
@@ -1,14 +1,14 @@
 # Ensure that each module can accessed explicitly as `m`; e.g.,
 # `artiruno.m.vda` will get the `vda` module. This is useful because
 # `artiruno.vda` will be set to a function instead of the module.
-import artiruno.preorder, artiruno.vda, artiruno.util
+import importlib
 class C: pass
 m = C()
-for s in ('preorder', 'vda', 'util'):
-    setattr(m, s, getattr(artiruno, s))
+for s in ('util', 'preorder', 'vda'):
+    setattr(m, s, importlib.import_module('artiruno.' + s))
 
 # Now do the ordinary imports.
+from artiruno.util import *
 from artiruno.preorder import (
     PreorderedSet, IC, EQ, LT, GT, ContradictionError)
 from artiruno.vda import vda
-from artiruno.util import *
