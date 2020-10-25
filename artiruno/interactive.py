5c2824861d0df44dd466aedba43a4bb7e6c7699f
Kodi Arfer <git@arfer.net>
2020-10-25 15:49:50 -0400

    [interactive] Display alt names.

--- artiruno/interactive.py
+++ artiruno/interactive.py
@@ -2,18 +2,21 @@ import sys, json
 from artiruno.preorder import LT, EQ, GT
 from artiruno.vda import vda
 
-def interact(criterion_names, **kwargs):
+def interact(criterion_names, alts, alt_names, **kwargs):
 
     def asker(a, b):
         print('\nWhich do you prefer?')
 
         options = dict(a = GT, b = LT, c = EQ)
         for k, v in sorted(options.items()):
-            print(f'\n({k})')
+            item = {GT: a, LT: b, EQ: None}[v]
+            print(f'\n({k})' + (
+                ' <<< ' + alt_names[alts.index(item)]
+                    if item in alts else ''))
             if v == EQ:
                 print('  The two options are equally preferable')
             else:
-                for name, value in zip(criterion_names, a if v == GT else b):
+                for name, value in zip(criterion_names, item):
                     print(f'- {name}: {value}')
 
         print('')
@@ -28,23 +31,30 @@ def interact(criterion_names, **kwargs):
             except KeyError:
                 print('Invalid input.')
 
-    return vda(asker = asker, **kwargs)
+    return vda(asker = asker, alts = alts, **kwargs)
 
 def main():
     with open(sys.argv[1]) as o:
         scenario = json.load(o)
 
     if alts := scenario.get('alts'):
+        alts = list(alts.items())
+        alt_names = tuple(k for k, _ in alts)
         alts = tuple(tuple(a[c] for c in scenario['criteria'])
-            for a in scenario['alts'].values())
+            for _, a in alts)
 
     prefs = interact(
         criterion_names = list(scenario['criteria']),
         criteria = list(scenario['criteria'].values()),
+        alt_names = alt_names,
         alts = alts,
         find_best = scenario.get('find_best'),
         max_dev = 2 * len(scenario['criteria']))
 
-    print("Best:", prefs.maxes(alts))
+    print("Best:", ", ".join(
+        alt_names[alts.index(a)]
+        for a in prefs.maxes(alts)))
 
-    prefs.get_subset(alts).render('/tmp/graph', format = 'png', view = True)
+    (prefs.get_subset(alts)
+        .graph(namer = lambda a: alt_names[alts.index(a)])
+        .render(filename = '/tmp/graph', format = 'png', view = True))
