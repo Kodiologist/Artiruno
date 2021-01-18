d238f69009439e5bf3e93a578ae0f80ff0071cc7
Kodi Arfer <git@arfer.net>
2021-01-18 11:02:58 -0500

    [setup.py] Fixed trove classifiers.

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
     packages = setuptools.find_packages(),
     classifiers = [
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
-        'Operating System :: OS Independent'],
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering'],
     python_requires = '>=3.8')
